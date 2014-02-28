28485cc5276a731c0e96a1e81182255b727bc892
Luka Penger <luka.penger@gmail.com>
2014-02-28 11:41:56 +0100

    uart 0 1

--- Libraries/UART.h
+++ Libraries/UART.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 	Luka Penger 2013
-	UART
+	I2C
 	LPC17xx
 ****************************************************************************/
 #ifndef __UART_H 
@@ -10,8 +10,6 @@
 #include "type.h"
 
 #define UART_BUFSIZE 		64
-
-#define UART_CLK        	100000000     /* Serial Peripheral Clock is 100 MHz */
 
 #define UART_IER_RBR		0x01
 #define UART_IER_THRE		0x02
@@ -32,49 +30,14 @@
 #define UART_LSR_TEMT		0x40
 #define UART_LSR_RXFE		0x80
 
-/* Parity enumerator */
-typedef enum {
-  UART_PARITY_NONE    = 0,
-  UART_PARITY_ODD     = 1,
-  UART_PARITY_EVEN    = 2,
-  UART_PARITY_MARK    = 3,
-  UART_PARITY_SPACE   = 4
-} UART_Parity;
-
-/* Stop Bits enumerator */
-typedef enum {
-  UART_STOP_BITS_1    = 0,
-  UART_STOP_BITS_1_5  = 1,
-  UART_STOP_BITS_2    = 2
-} UART_StopBits;
-
-/* Data Bits enumerator */
-typedef enum {
-  UART_DATA_BITS_5    = 5,
-  UART_DATA_BITS_6    = 6,
-  UART_DATA_BITS_7    = 7,
-  UART_DATA_BITS_8    = 8
-} UART_DataBits;
-
-/* UART Port Properties structure */
-typedef struct {
-  uint32_t           Baudrate;
-  UART_DataBits      DataBits;
-  UART_Parity        Parity;
-  UART_StopBits      StopBits;
-} UART_Configuration;
-
 void UART_RBR_Disable(char portNum);
 void UART_RBR_Enable(char portNum);
-void UART_Init(char portNum, UART_Configuration configuration);
+BOOL UART_Init(char portNum, unsigned int baudRate);
 void UART_SendChar(char portNum, char value);
 void UART_SendBuffer(char portNum, char *buffer, unsigned int length);
 char UART_GetChar(char portNum);
-void UART_SetConfiguration(char portNum, UART_Configuration configuration);
 extern void UART0_IRQHandler(void);
 extern void UART1_IRQHandler(void);
-extern void UART2_IRQHandler(void);
-extern void UART3_IRQHandler(void);
 
 #endif /* end __UART_H */
 /***************************************************************************
